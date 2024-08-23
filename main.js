8e3bc6d75da8b8d07ad92e38aa92f1314e98a357
vamsi-kanakam <vamsikanakam2002@gmail.com>
2024-08-22 21:16:57 -0500

    crawl single page with error handling

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log(`Finally Back on Track!!`);
+const {crawlPage} = require('./crawl.js');
+
+function main(){
+    if(process.argv.length < 3){
+        console.log(`No website provided`)
+        process.exit(1);
+    }
+    if(process.argv.length > 3){
+        console.log(`Too many command line args`)
+        process.exit(1);
+    }
+    const baseURL = process.argv[2];
+    console.log(`starting crawl! of ${baseURL}`);
+
+    crawlPage(baseURL);
+}
+
+main()
